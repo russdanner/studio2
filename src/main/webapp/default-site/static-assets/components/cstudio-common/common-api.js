2fadcbb136e1f57b0fd5a9bcb51bfdd5fb2161ff
Hyanghee Lim <hlim@rivetlogic.com>
2014-11-17 17:29:41 -0500

    removed changeWcmContentTemplateServiceUri and its use since we are using changeContentTypeUrl instead

--- src/main/webapp/default-site/static-assets/components/cstudio-common/common-api.js
+++ src/main/webapp/default-site/static-assets/components/cstudio-common/common-api.js
@@ -2013,7 +2013,7 @@ YConnect.failureEvent.subscribe(function() {
             lookupContentDependenciesServiceUri: "/proxy/alfresco/cstudio/wcm/dependency/get-dependencies?deletedep=true&",
     
 			wcmMapContentServiceUri: "/proxy/alfresco/cstudio/wcm/content/map-content",
-			changeWcmContentTemplateServiceUri: "/proxy/alfresco/cstudio/wcm/content/add-wcm-properties",
+
 			copyContentToClipboardServiceUri: "/api/1/services/clipboard/copy-item.json",
 			cutContentToClipboardServiceUri: "/api/1/services/clipboard/cut-item.json",
 			pasteContentFromClipboardServiceUri: "/api/1/services/clipboard/paste-item.json",
@@ -2569,25 +2569,6 @@ YConnect.failureEvent.subscribe(function() {
 				YConnect.asyncRequest('POST', this.createServiceUri(serviceUrl), serviceCallback, JSON.stringify(taxonomies));
             },
             
-			/**
-			 * change the content type/template for a given wcm object
-			 */
-			changeWcmContentTemplate: function(site, author, contentPath, contentType, changeTemplateCb) {
-				var serviceUrl = this.changeWcmContentTemplateServiceUri;
-				serviceUrl += "?site=" + site;
-				serviceUrl += "&path=" + contentPath;
-				serviceUrl += "&contentType=" + contentType;
-				serviceUrl += "&author=" + author;
-				var serviceCallback = {
-					success: function(response) {
-						changeTemplateCb.success();
-					},
-					failure: function(response) {
-						changeTemplateCb.failure(response);
-					}
-				};
-				YConnect.asyncRequest('GET', this.createServiceUri(serviceUrl), serviceCallback);
-			},
 			/**
 			 * change template functionality. 
 			 */
